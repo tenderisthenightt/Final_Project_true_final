29552c21b867dff1d58ef667c1f4daadf67d0333
Donghwan Jung <yiijk700@gmail.com>
2023-02-13 20:53:43 +0900

    a

--- mci/game/5_pygame/5app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def memory():
-    return render_template('5index.html')
-
-if __name__ == '__5main__':
-    app.run(debug=True)
