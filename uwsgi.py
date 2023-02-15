433d362acf21b308f557cc6b8ad3fc1dd5356347
tenderisthenightt <120777172+tenderisthenightt@users.noreply.github.com>
2023-02-15 09:52:00 +0900

    Co-authored-by: huiyahuny <huiyahuny@users.noreply.github.com>

--- uwsgi.py
+++ uwsgi.py
@@ -2,4 +2,4 @@ from mci import app
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=7777, ssl_context=("fullchain1.pem", 'privkey1.pem'))
+    app.run(host='0.0.0.0', debug=True, port=7777, ssl_context=("fullchain1.pem", 'privkey1.pem'))
